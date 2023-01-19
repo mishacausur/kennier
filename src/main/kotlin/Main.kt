dd4c6e443a09207e30530a04cc7e2af086dde184
Misha Causur <mishacausur@mail.ru>
2023-01-20 00:49:50 +0500

    first task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+   makeSmth("Project started")
+    sumSmth(320, 390)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun makeSmth(text: String) {
+    (text).also {
+        println(it)
+    }
+}
+
+fun sumSmth(firstNumber: Int, secondNumber: Int) {
+    println("result of $firstNumber + $secondNumber is ${firstNumber + secondNumber}")
 }
