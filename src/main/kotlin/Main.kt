b7b92abe52f16423961943108b0b1fbc0e7aa8f1
Misha Causur <mishacausur@mail.ru>
2023-01-22 00:42:13 +0500

    collections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,21 @@
 fun main() {
-enterPoint(false)
+listChecker()
 }
 
-fun enterPoint(isNow: Boolean) {
-    if (isNow == true) before() else after()
+fun listChecker() {
+    val cities = listOf<String>("Los_Angeles", "New-York", "Berlin", "London")
+    cities
+        .filter { it.startsWith("L") }
+        .sortedBy { it }
+        .map { it.uppercase() }
+        .forEach {(println(it))}
 }
 
-fun after() {
-    val input = readLine()
-    println("you have entered: ${input?.uppercase()}")
-    val array = arrayOf("one", "two", "three")
-    array.forEach { println(it) }
-}
-
-fun before() {
-    makeSmth("Project started")
-    sumSmth(320, 390)
-    stringer()
-}
-
-fun makeSmth(text: String) {
-    (text).also {
-        println(it)
+fun arrayChecker() {
+    val array = arrayOf("one", "two", "three", "four")
+    /* checks the only first */
+    when {
+        "two" in array -> println("there is 2 in array")
+        "four" in array -> println("and now it has four")
     }
-}
-
-fun sumSmth(firstNumber: Int, secondNumber: Int) {
-    println("result of $firstNumber + $secondNumber is ${firstNumber + secondNumber}")
-}
-
-fun stringer() {
-    val str = "string powerful"
-    println(
-        "string is now ${
-            str
-        .uppercase()
-        .lowercase()
-    }"
-    )
 }
